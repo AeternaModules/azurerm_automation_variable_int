240a0c964d0527e22c4ec533a56ca4986dda347a
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "automation_variable_ints_id" {
   description = "Map of id values across all automation_variable_ints, keyed the same as var.automation_variable_ints"
-  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.id }
+  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "automation_variable_ints_automation_account_name" {
   description = "Map of automation_account_name values across all automation_variable_ints, keyed the same as var.automation_variable_ints"
-  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.automation_account_name }
+  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.automation_account_name if v.automation_account_name != null && length(v.automation_account_name) > 0 }
 }
 output "automation_variable_ints_description" {
   description = "Map of description values across all automation_variable_ints, keyed the same as var.automation_variable_ints"
-  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.description }
+  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "automation_variable_ints_encrypted" {
   description = "Map of encrypted values across all automation_variable_ints, keyed the same as var.automation_variable_ints"
-  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.encrypted }
+  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.encrypted if v.encrypted != null }
 }
 output "automation_variable_ints_name" {
   description = "Map of name values across all automation_variable_ints, keyed the same as var.automation_variable_ints"
-  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.name }
+  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "automation_variable_ints_resource_group_name" {
   description = "Map of resource_group_name values across all automation_variable_ints, keyed the same as var.automation_variable_ints"
-  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.resource_group_name }
+  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "automation_variable_ints_value" {
   description = "Map of value values across all automation_variable_ints, keyed the same as var.automation_variable_ints"
-  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.value }
+  value       = { for k, v in azurerm_automation_variable_int.automation_variable_ints : k => v.value if v.value != null }
 }
 
